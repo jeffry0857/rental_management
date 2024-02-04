da7347e02b0df0025b3c9365842237cba6d6c01e
Jeffry <jeffry.lin@egk.com.tw>
2024-02-05 07:21:00 +0800

    Remove AboutView, add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <div v-if="user">
+          <button>Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import useLogout from '@/composables/useLogout';
+import getUser from '@/composables/getUser';
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const { user } = getUser()
+    const { logout } = useLogout()
+    const router = useRouter()
+
+    const handleClick = async () => {
+      await logout()
+      router.push({ name: 'Login'})
+    }
+
+    return { handleClick, user }
+  }
+}
+</script>
+
+<style scoped>
+  .navbar {
+    padding: 16px 10px;
+    margin-bottom: 60px;
+    background: white;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  nav h1 {
+    margin-left: 20px;
+  }
+  nav .links {
+    margin-left: auto;
+  }
+  nav .links a, button {
+    margin-left: 16px;
+    font-size: 14px;
+  }
+</style>

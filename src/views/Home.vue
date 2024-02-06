8207735062809022ff1351d2c5bcb3c2456c3ec5
Jeffry <jeffry.lin@egk.com.tw>
2024-02-06 23:09:50 +0800

    Rename: Home, Signup layout completed

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div v-if="error" class="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import getCollection from '@/composables/getCollection'
+
+export default {
+  name: 'Home',
+  setup() {
+    const { error, documents } = getCollection('playlists')
+
+    return { error, documents }
+  }
+}
+</script>

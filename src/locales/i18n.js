167eea85959c4e37ee329ba54c62aaa6a72350f1
Jeffry <jeffry.lin@egk.com.tw>
2024-09-04 19:38:58 +0800

    新增多語系 i18n Config

---
+++ src/locales/i18n.js
@@ -0,0 +1,32 @@
+import { createI18n } from "vue-i18n";
+
+// Translation definition
+const messages = {
+  en: {
+    message: {
+      login: "Login",
+      logout: "Log out",
+      signup: "Sign up",
+      upload_room_photo: "Upload Room Photo",
+    },
+  },
+  tw: {
+    message: {
+      login: "登入",
+      logout: "登出",
+      signup: "註冊",
+      upload_room_photo: "上傳房間照片",
+    },
+  },
+};
+
+// Create i18n instance
+const i18n = createI18n({
+  locale: "tw", // 設定本地語系
+  fallbackLocale: "tw", // 設定首選語言缺少翻譯時要使用的語言
+  messages, // 語系包
+  legacy: false,
+  globalInjection: true, // 該屬性可讓所有元件中注入前綴為`$`的全域屬性 & 方法
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { projectAuth } from "./firebase/config";
-
+import i18n from "./locales/i18n";
 import "./assets/main.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 
 let app;
 
 projectAuth.onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App).use(store).use(router).mount("#app");
+    app = createApp(App)
+      .use(i18n)
+      .use(vuetify)
+      .use(store)
+      .use(router)
+      .mount("#app");
   }
 });
